b1ad11a22369197e586c5d5bb91b0820d0293b23
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-06-26 21:48:19 -0300

    Fixing the new game overview screen.

--- futtoboru-core/src/main/java/com/rndmodgames/futtoboru/data/Club.java
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/data/Club.java
@@ -160,6 +160,31 @@ public class Club implements Serializable {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Club other = (Club) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         return name;
     }
