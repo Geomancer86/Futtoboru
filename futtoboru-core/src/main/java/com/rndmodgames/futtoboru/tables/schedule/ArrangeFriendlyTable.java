753a4a6f2fed92b58a23118f03be5b34cf8ac09f
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-04 11:17:56 -0300

    Let the user pick match type, venue and opponent club before arranging a friendly match.

--- futtoboru-core/src/main/java/com/rndmodgames/futtoboru/tables/schedule/ArrangeFriendlyTable.java
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/tables/schedule/ArrangeFriendlyTable.java
@@ -1,19 +1,44 @@
 package com.rndmodgames.futtoboru.tables.schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisSelectBox;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
 import com.rndmodgames.futtoboru.data.Club;
 import com.rndmodgames.futtoboru.game.Futtoboru;
+import com.rndmodgames.futtoboru.system.DatabaseLoader;
 import com.rndmodgames.futtoboru.system.EngineParameters;
 import com.rndmodgames.futtoboru.system.SaveGame;
 import com.rndmodgames.localization.LanguageModLoader;
 
 /**
  * Arrange Friendly Table v1
+ *  
+ *  TODO WIP:
  * 
+ *  - ARRANGE FRIENDLY COMPONENT TITLE
+ *  
+ *  - Match Type Select (Friendly, Cup, Home and Away, etc) NOTE: only Friendly for this release
+ *  - Date Picker (date might be selected directly/manually by clicking on the Fixtures Component when that portion is implemented
+ *  - Venue (Home, Away, Neutral) needs stadiums to be implemented.
+ *  - Match Rules (90 minutes only, penalties, etc.) NOTE: Only 90 minutes for this release
+ *  
+ *  - SELECTED DATE LABEL TITLE
+ *  
+ *  - Selected Team
+ *  
+ *  - Expected Fee & Income Widget
+ *      - A good opportunity to get more data on this and the reasons for the fees & income
+ *      - Might show a color or reaction score for the opponent (classic matches get more income)
+ *      - Show the expected attendance (this might calculate other important matches the same day)
+ *      
  * @author Geomancer86
  */
 public class ArrangeFriendlyTable extends VisTable {
@@ -29,8 +54,14 @@ public class ArrangeFriendlyTable extends VisTable {
     String selectedMatchType = null;
     String selectedVenueType = null;
     String selectedMatchRulesType = null;
+    Club selectedClub = null;
+    Club selectedOpponentClub = null;
     
     // Dynamic Components
+    VisLabel feeValueLabel = new VisLabel("$0");
+    VisLabel incomeValueLabel = new VisLabel("$0");
+    VisSelectBox<Club> opponentSelectBox = new VisSelectBox<Club>();
+    VisTextButton confirmFriendlyButton = new VisTextButton(LanguageModLoader.getValue("confirm"));
     
     public ArrangeFriendlyTable(Futtoboru parent) {
         
@@ -67,14 +98,18 @@ public class ArrangeFriendlyTable extends VisTable {
         
         matchRulesSelectBox.setSelectedIndex(0);
         selectedMatchRulesType = matchRulesSelectBox.getSelected();
-        
+
         /**
-         * List of Clubs (do not include current club)
-         * 
-         *  - TODO: this component is difficult because should allow to do some filtering at least
-         *  - TODO: component shows opponent level/fame
+         * Club Selectbox
          */
-        VisSelectBox<String> opponentSelectBox = new VisSelectBox<String>();
+        opponentSelectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                
+                // Disable Confirm if no Opponent Selected
+                confirmFriendlyButton.setDisabled(opponentSelectBox.getSelected().getId() == null);
+            }
+        });
         
         //
         this.row();
@@ -108,13 +143,18 @@ public class ArrangeFriendlyTable extends VisTable {
         this.add("Fee");
         this.add("Income");
         
+        //
+        this.row();
+        this.add(feeValueLabel);
+        this.add(incomeValueLabel);
+        
         /**
          * Confirm Friendly Button  (adds Friendly Request)
          * Cancel Button            (clear forms)
          */
-        VisTextButton confirmFriendlyButton = new VisTextButton(LanguageModLoader.getValue("confirm"));
         VisTextButton cancelFriendlyButton = new VisTextButton(LanguageModLoader.getValue("cancel"));
         
+        // Confirm Friendly Listener
         confirmFriendlyButton.addCaptureListener(new InputListener() {
             
             @Override
@@ -130,6 +170,7 @@ public class ArrangeFriendlyTable extends VisTable {
                 System.out.println("selectedMatchType:      " + selectedMatchType);
                 System.out.println("selectedVenueType:      " + selectedVenueType);
                 System.out.println("selectedMatchRulesType: " + selectedMatchRulesType);
+                System.out.println("selectedOpponentClub    : " + opponentSelectBox.getSelected());
             }
         });
         
@@ -149,37 +190,58 @@ public class ArrangeFriendlyTable extends VisTable {
         });
         
         // Confirm Friendly Button Disabled Until Valid Form
-//        confirmFriendlyButton.setDisabled(true);
+        confirmFriendlyButton.setDisabled(true);
         
         this.row().padTop(100);
         this.add(confirmFriendlyButton).bottom();
         this.add(cancelFriendlyButton).bottom();
         
-        /**
-         * TODO WIP:
-         * 
-         *  - ARRANGE FRIENDLY COMPONENT TITLE
-         *  
-         *  - Match Type Select (Friendly, Cup, Home and Away, etc) NOTE: only Friendly for this release
-         *  - Date Picker (date might be selected directly/manually by clicking on the Fixtures Component when that portion is implemented
-         *  - Venue (Home, Away, Neutral) needs stadiums to be implemented.
-         *  - Match Rules (90 minutes only, penalties, etc.) NOTE: Only 90 minutes for this release
-         *  
-         *  - SELECTED DATE LABEL TITLE
-         *  
-         *  - Selected Team
-         *  
-         *  - Expected Fee & Income Widget
-         *      - A good opportunity to get more data on this and the reasons for the fees & income
-         *      - Might show a color or reaction score for the opponent (classic matches get more income)
-         *      - Show the expected attendance (this might calculate other important matches the same day)
-         *      
-         */
+        //
+        updateDynamicComponents();
     }
     
     //
     public void updateDynamicComponents() {
         
+        // 
+        if (currentClub != null) {
+            
+            /**
+             * List of Clubs (do not include current club)
+             * 
+             *  - TODO: this component is difficult because should allow to do some filtering at least
+             *  - TODO: component shows opponent level/fame
+             *  
+             *  ---
+             *  TODO unhardcode available clubs for friendly
+             *  TODO filter out current club
+             *  TODO filter out clubs with matches on the selected day so they cannot play/accept other match
+             */
+            
+            // TODO UNHARDCODE THIS BEFORE WE IMPLEMENT MORE TEAMS/LEAGUES
+            List<Club> availableClubs = new ArrayList<>();
+            
+            // 
+            availableClubs.add(new Club("CHOOSE"));
+            
+            //
+            availableClubs.addAll(DatabaseLoader.getClubsByCountry().get(1000L));
+            
+            // Remove Current Club Hack
+            for (Club club : availableClubs) {
+                
+                // Ignore null id club (CHOOSE item)
+                if (club.getId() != null
+                        && club.getId().equals(currentClub.getId())) {
+                    
+                    //
+                    availableClubs.remove(club);
+                    break;
+                }
+            }
+                    
+            opponentSelectBox.setItems(availableClubs.toArray(new Club[availableClubs.size()]));
+        }
     }
 
     public Club getCurrentClub() {
