11a6ceb5f114fd51a5a9d696e59ed6bc2d25f41e
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-04 10:50:47 -0300

    Add a basic engine parameters class with some hardcoded form values, for the Friendly Match creator component.

---
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/system/EngineParameters.java
@@ -0,0 +1,18 @@
+package com.rndmodgames.futtoboru.system;
+
+/**
+ * Engine Parameters v1
+ * 
+ * @author Geomancer86
+ */
+public class EngineParameters {
+
+    //
+    public static String [] matchTypes = new String [] {"Friendly Match"};
+    
+    //
+    public static String [] matchVenueTypes = new String [] {"Home", "Away", "Neutral"};
+    
+    //
+    public static String [] matchRulesTypes = new String [] {"90 Minutes Only"};
+}
