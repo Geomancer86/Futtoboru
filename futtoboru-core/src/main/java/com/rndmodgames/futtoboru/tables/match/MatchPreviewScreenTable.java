f5fbb48a5be96780243b93f90149c46384c02534
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-08 15:03:05 -0300

    Add the basic match preview screen placeholder.

---
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/tables/match/MatchPreviewScreenTable.java
@@ -0,0 +1,23 @@
+package com.rndmodgames.futtoboru.tables.match;
+
+import com.kotcrab.vis.ui.widget.VisTable;
+
+/**
+ * Match Preview Screen Table v1
+ * 
+ *  - basic:
+ *      - home and away clubs
+ *      - venue
+ *      - tickets/attendance/money
+ *      - basic players/squads/lineups/etc
+ *      - estimated odds
+ *      
+ *      getting into this screen will change the button to MATCH RESULT
+ *          
+ *      after match result screen gets back to CONTINUE GAME
+ * 
+ * @author Geomancer86
+ */
+public class MatchPreviewScreenTable extends VisTable {
+
+}
