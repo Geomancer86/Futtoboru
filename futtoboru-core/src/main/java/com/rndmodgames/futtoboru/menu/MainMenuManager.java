361732162adaf073810f0e3f491be51d58a1fd15
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-12 12:04:20 -0300

    Add the automatic dynamic screen update to the squad screen when is the current screen (to update the average and individual players ages).

--- futtoboru-core/src/main/java/com/rndmodgames/futtoboru/menu/MainMenuManager.java
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/menu/MainMenuManager.java
@@ -387,10 +387,16 @@ public class MainMenuManager {
     
     /**
      * Allow to call this from other places after data is updated
+     * 
+     *  - this is used to automatically update the current screen, as it will have dynamic data
      */
     public void updateDynamicComponents() {
         
         switch(CURRENT_SCREEN) {
+        
+        case MAIN_SQUAD_SCREEN:
+            squadScreenTable.updateDynamicComponents();
+            break;
         
         case AUTHORITY_SCREEN:
             authorityScreenTable.updateDynamicComponents();
