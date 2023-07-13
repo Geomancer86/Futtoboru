28a1060de7a754d7e307068dc5d1ba3994c2164f
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-13 12:45:17 -0300

    Add two base unit tests for stadiums and player loaders.

---
+++ futtoboru-core/src/test/java/com/rndmodgames/futtoboru/system/loaders/StadiumsLoaderTests.java
@@ -0,0 +1,60 @@
+package com.rndmodgames.futtoboru.system.loaders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.rndmodgames.futtoboru.data.Club;
+import com.rndmodgames.futtoboru.data.Country;
+import com.rndmodgames.futtoboru.game.Futtoboru;
+import com.rndmodgames.futtoboru.system.DatabaseLoader;
+import com.rndmodgames.futtoboru.system.SaveGame;
+import com.rndmodgames.futtoboru.system.generators.PersonGenerator;
+
+class StadiumsLoaderTests {
+
+    private static Futtoboru application;
+    
+    @BeforeAll
+    static void beforeAll() {
+        
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        config.updatesPerSecond = 30;
+        application = new Futtoboru();
+        
+        // Initialize the Saved Game
+        application.setCurrentGame(new SaveGame());
+        
+        // Set the Person Generator
+        application.setPersonGenerator(new PersonGenerator(application));
+        
+        new HeadlessApplication(application , config);
+    }
+    
+    @Test
+    void loadStadiumsTest() {
+        
+        DatabaseLoader dbLoader = DatabaseLoader.getInstance();
+        
+        assertNotNull(dbLoader);
+        
+        /**
+         * Iterate all clubs and make sure all stadiums are there
+         */
+        Country england = DatabaseLoader.getCountryById(1000L);
+        
+        List<Club> allClubs = dbLoader.getClubsByCountry(england);
+        
+        for (Club club : allClubs) {
+            
+            assertNotNull(club.getStadium());
+        }
+        
+        System.out.println("All " + allClubs.size() + " Have a Stadium File.");
+    }
+}
