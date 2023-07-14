e97af2229e2c00bdc9b93d0764accf500fdd59b2
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2023-07-14 14:20:44 -0300

    Competition history loader work in progress.

---
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/system/loaders/CompetitionsLoader.java
@@ -0,0 +1,20 @@
+package com.rndmodgames.futtoboru.system.loaders;
+
+import com.rndmodgames.futtoboru.data.Season;
+
+/**
+ * Competitions Loader v1
+ * 
+ *  - Load existing competitions, history and rules from file system
+ * 
+ * @author Geomancer86
+ */
+public class CompetitionsLoader {
+
+    /**
+     * Loads the existing Competitions Data
+     */
+    public static void loadCompetitions(Season season) {
+        
+    }
+}
