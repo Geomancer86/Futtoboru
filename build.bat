b1c9ea7af6a578f14755d2ccbcd84168193c649f
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2025-12-14 22:10:00 -0300

    fix: Use hardcoded Maven path C:\\apache-maven-3.6.3

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 title Futtoboru Build
 echo Building project...
 echo.
-call mvn clean install -DskipTests
+call C:\apache-maven-3.6.3\bin\mvn.cmd clean install -DskipTests
 echo.
 echo Build complete. Press any key to close...
 pause >nul
