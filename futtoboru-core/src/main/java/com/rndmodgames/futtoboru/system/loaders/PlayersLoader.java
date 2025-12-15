b5fa0240b2383d914b821b79c9919baa17696baf
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2025-12-15 17:26:23 -0300

    fix: Correct indentation in catch block - Fixed indentation of variables and code inside else block - Resolves syntax errors from previous commit

--- futtoboru-core/src/main/java/com/rndmodgames/futtoboru/system/loaders/PlayersLoader.java
+++ futtoboru-core/src/main/java/com/rndmodgames/futtoboru/system/loaders/PlayersLoader.java
@@ -167,51 +167,52 @@ public class PlayersLoader {
                                                  ", using randomized date: " + year + "-" + month + "-" + day);
                             } else {
                                 String dateStr = splitted[4].trim();
-                            int year;
-                            int month;
-                            int day;
-                            
-                            // Try to extract year from the date string
-                            if (dateStr.contains("-")) {
-                                // Has dashes, try to parse year-month or year-month-day
-                                String[] dateParts = dateStr.split("-");
-                                if (dateParts.length >= 1) {
-                                    year = Integer.valueOf(dateParts[0]);
-                                } else {
-                                    year = 1865; // Default fallback year
-                                }
+                                int year;
+                                int month;
+                                int day;
                                 
-                                if (dateParts.length >= 2) {
-                                    // Month is provided
-                                    month = Integer.valueOf(dateParts[1]);
+                                // Try to extract year from the date string
+                                if (dateStr.contains("-")) {
+                                    // Has dashes, try to parse year-month or year-month-day
+                                    String[] dateParts = dateStr.split("-");
+                                    if (dateParts.length >= 1) {
+                                        year = Integer.valueOf(dateParts[0]);
+                                    } else {
+                                        year = 1865; // Default fallback year
+                                    }
+                                    
+                                    if (dateParts.length >= 2) {
+                                        // Month is provided
+                                        month = Integer.valueOf(dateParts[1]);
+                                    } else {
+                                        // Randomize month
+                                        month = DatabaseLoader.RNG.nextInt(12) + 1;
+                                    }
                                 } else {
-                                    // Randomize month
+                                    // Just a year
+                                    year = Integer.valueOf(dateStr);
                                     month = DatabaseLoader.RNG.nextInt(12) + 1;
                                 }
-                            } else {
-                                // Just a year
-                                year = Integer.valueOf(dateStr);
-                                month = DatabaseLoader.RNG.nextInt(12) + 1;
+                                
+                                // Randomize day based on month
+                                switch(month) {
+                                case 1,3,5,7,8,10,12:
+                                    day = DatabaseLoader.RNG.nextInt(31) + 1;
+                                    break;
+                                case 2:
+                                    day = DatabaseLoader.RNG.nextInt(28) + 1;
+                                    break;
+                                default:
+                                    day = DatabaseLoader.RNG.nextInt(30) + 1;
+                                    break;
+                                }
+                                
+                                // Set random birthday
+                                person.setBirthDate(LocalDate.of(year, month, day).atStartOfDay());
+                                
+                                System.out.println("WARNING: Could not parse birthdate '" + dateStr + "' for " + person.getName() + " " + person.getLastname() + 
+                                                 ", using randomized date: " + year + "-" + month + "-" + day);
                             }
-                            
-                            // Randomize day based on month
-                            switch(month) {
-                            case 1,3,5,7,8,10,12:
-                                day = DatabaseLoader.RNG.nextInt(31) + 1;
-                                break;
-                            case 2:
-                                day = DatabaseLoader.RNG.nextInt(28) + 1;
-                                break;
-                            default:
-                                day = DatabaseLoader.RNG.nextInt(30) + 1;
-                                break;
-                            }
-                            
-                            // Set random birthday
-                            person.setBirthDate(LocalDate.of(year, month, day).atStartOfDay());
-                            
-                            System.out.println("WARNING: Could not parse birthdate '" + dateStr + "' for " + person.getName() + " " + person.getLastname() + 
-                                             ", using randomized date: " + year + "-" + month + "-" + day);
                         }
                         
                         /**
